c677842d0718c77dfbdd5c6319eaba3cbe37b584
kikashy <kikashy@hotmail.com>
2024-11-07 22:32:11 -0500

    Enhance foreach_batch_function to validate rows against expectations and log results, separating valid and violating records for improved data handling

--- src/schemon_python_client/spark/helper/databricks.py
+++ src/schemon_python_client/spark/helper/databricks.py
@@ -1,6 +1,8 @@
 from databricks.sdk.runtime import *  # type: ignore
-from pyspark.sql import DataFrame as SparkDataFrame
-from schemon_python_logger.logger import Logger
+from pyspark.sql import DataFrame as SparkDataFrame, functions as F
+from schemon_python_expectation.spark.expectation.column_expectation import (
+    ColumnExpectation,
+)
 
 
 def get_secret_value(scope: str, key: str) -> str:
@@ -27,15 +29,58 @@ def foreach_batch_function(
     batch_df: SparkDataFrame,
     epoch_id: str,
     target_table: str,
+    bad_row_path: str,
+    expectations: dict = None,
 ):
-    # Get distinct original file names from the batch
-    file_names = batch_df.select("Source").distinct().rdd.flatMap(lambda x: x).collect()
+    valid_rows = None
+    violations = None
+    if expectations:
+        # Generate validation checks and violation messages in parallel
+        validation_checks = [
+            F.when(
+                ~getattr(ColumnExpectation, rule)(column),
+                F.lit(f"Column {column} failed '{rule}'"),
+            ).otherwise(F.lit(None))
+            for column, rule in expectations.items()
+        ]
 
-    # Perform the write operation
-    batch_df.write.format("delta").mode("append").saveAsTable(target_table)
+        # Create a single DataFrame with a violation message column
+        checked_df = batch_df.withColumn(
+            "violation_message",
+            F.concat_ws(
+                ", ", *[check for check in validation_checks if check is not None]
+            ),
+        )
 
-    # Print a custom message after the batch is written, including the original file names
+        # Separate valid and invalid rows using a single transformation
+        valid_rows = checked_df.filter(
+            "violation_message IS NULL OR violation_message == ''"
+        )
+        violations = checked_df.filter(
+            "violation_message IS NOT NULL AND violation_message <> ''"
+        )
+    else:
+        valid_rows = batch_df
+
+    # Log the batch processing results
     num_records = batch_df.count()
-    log_message = f"Batch {epoch_id} processed and written to {target_table} with {num_records} records from files: {', '.join(file_names)}"
+    num_valid = valid_rows.count() if valid_rows else 0
+    num_violations = violations.count() if violations else 0
+
+    # Write valid and violation rows as before
+    if num_valid > 0:
+        valid_rows = valid_rows.drop("violation_message")
+        valid_rows.write.format("delta").mode("append").saveAsTable(target_table)
+    if num_violations > 0:
+        bad_row_path = f"{bad_row_path}/{target_table}"
+        violations.write.format("parquet").mode("append").save(bad_row_path)
+
+    log_message = f"Batch {epoch_id} | {num_records} records processed."
+
+    if expectations:
+        log_message += (
+            f"\nBatch {epoch_id} | {num_valid} valid records written to {target_table}."
+        )
+        log_message += f"\nBatch {epoch_id} | {num_violations} violating records redirected to {bad_row_path}."
 
     print(log_message)
